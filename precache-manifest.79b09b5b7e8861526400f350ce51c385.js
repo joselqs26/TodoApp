22d3d08c85147978185e41aa905042ec052b4551
Jose Luis Quintero <joseluisquintero06@gmail.com>
2022-08-10 18:12:25 +0000

    Updates

---
+++ precache-manifest.79b09b5b7e8861526400f350ce51c385.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4629828be3f703e616b",
+    "url": "/TodoApp/static/css/main.ec692755.chunk.css"
+  },
+  {
+    "revision": "c4629828be3f703e616b",
+    "url": "/TodoApp/static/js/main.c4629828.chunk.js"
+  },
+  {
+    "revision": "84a748a05bbc3012460c",
+    "url": "/TodoApp/static/js/1.84a748a0.chunk.js"
+  },
+  {
+    "revision": "d779971c9701e8dcfd5b",
+    "url": "/TodoApp/static/js/runtime~main.d779971c.js"
+  },
+  {
+    "revision": "cb70b8ea33518ac0e173b9976772c5fd",
+    "url": "/TodoApp/static/media/Check - Deactivate.cb70b8ea.svg"
+  },
+  {
+    "revision": "496477643b2c294bcdfced0119308bc2",
+    "url": "/TodoApp/static/media/Check.49647764.svg"
+  },
+  {
+    "revision": "698308475255ca227d414c702cb3294b",
+    "url": "/TodoApp/static/media/Clip.69830847.svg"
+  },
+  {
+    "revision": "4d4d7de08a574581b339c3c5de7a61f3",
+    "url": "/TodoApp/static/media/Remove.4d4d7de0.svg"
+  },
+  {
+    "revision": "cba5577736c20caab925c41a543acbe8",
+    "url": "/TodoApp/static/media/Search.cba55777.svg"
+  },
+  {
+    "revision": "a37a53016322dddb967082a5c28ec2b3",
+    "url": "/TodoApp/static/media/conjunto-vacio.a37a5301.png"
+  },
+  {
+    "revision": "32365832882f46c9a8489a039529f6a6",
+    "url": "/TodoApp/static/media/Logo - Dark.32365832.svg"
+  },
+  {
+    "revision": "2d0878af2529ee6eda03f990817962ad",
+    "url": "/TodoApp/index.html"
+  }
+];
